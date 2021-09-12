b18d103f8dfc814041ccc7591f7f5df6395f5cce
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-09-12 19:10:30 -0400

    app

---
+++ app/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/Home";
+import DetailScreen from "./src/Detail";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    options={{ headerShown: false }}
+                    component={HomeScreen}
+                />
+                <Stack.Screen name="Detail" component={DetailScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
